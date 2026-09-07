7ef58b1b239e744e4b6c76c5d4b31872ae06a235
Wabi Tafese <wabitafese9@gmail.com>
2026-09-07 15:01:29 +0300

    Restore Isarion source (artifacts/isarion/components/KeyboardAwareScrollViewCompat.tsx)

---
+++ artifacts/isarion/components/KeyboardAwareScrollViewCompat.tsx
@@ -0,0 +1,18 @@
+import { ScrollView, ScrollViewProps } from 'react-native';
+
+type Props = ScrollViewProps;
+
+export function KeyboardAwareScrollViewCompat({
+  children,
+  keyboardShouldPersistTaps = 'handled',
+  ...props
+}: Props) {
+  return (
+    <ScrollView
+      keyboardShouldPersistTaps={keyboardShouldPersistTaps}
+      {...props}
+    >
+      {children}
+    </ScrollView>
+  );
+}
